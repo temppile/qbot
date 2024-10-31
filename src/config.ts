ec051cfe0de617c617a15158cdc619ede35de1ba
temppile <temptemppile1@outlook.com>
2024-10-31 01:49:23 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,15 @@ import { ActivityType } from 'discord.js';
 import { BotConfig } from './structures/types'; 
 
 export const config: BotConfig = {
-    groupId: 0,
+    groupId: 34836147,
     slashCommands: true,
     legacyCommands: {
         enabled: true,
         prefixes: ['q!'],
     },
     permissions: {
-        all: [''],
-        ranking: [''],
+        all: ['1283150907831353425', '1235671893315424326', '1280174749816979456', '1282054590522720287', '1297360036670079027'],
+        ranking: ['1280145653464432660', '1189322992044478519', '1189323232973689002', '1300641917003894895', '1301027087301349420', '1189323180414865469'],
         users: [''],
         shout: [''],
         join: [''],
@@ -18,20 +18,20 @@ export const config: BotConfig = {
         admin: [''],
     },
     logChannels: {
-        actions: '',
-        shout: '',
+        actions: '1280281375445880874',
+        shout: '1280281375445880874',
     },
-    api: false,
+    api: true,
     maximumRank: 255,
-    verificationChecks: false,
-    bloxlinkGuildId: '',
+    verificationChecks: true,
+    bloxlinkGuildId: '1074357095488552991',
     firedRank: 1,
     suspendedRank: 1,
     recordManualActions: true,
     memberCount: {
-        enabled: false,
-        channelId: '',
-        milestone: 100,
+        enabled: true,
+        channelId: '1280292208012034121',
+        milestone: 200,
         onlyMilestones: false,
     },
     xpSystem: {
@@ -48,7 +48,7 @@ export const config: BotConfig = {
     activity: {
         enabled: false,
         type: ActivityType.Watching,
-        value: 'for commands.',
+        value: 'over Wingstop!',
     },
     status: 'online',
     deleteWallURLs: false,
